90e366bf6fe95cdadbb276f35612e96f0e908762
Kabil M <kabilm1012@gmail.com>
2024-10-12 17:44:20 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+import invoice
+
+invoice.generate("invoices", "PDFs", "msk.jpg", 
+                 "product_id", "product_name", "amount_purchased",
+                 "price_per_unit", "total_price")
