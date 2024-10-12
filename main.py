56add95ffabbd9f98a319726ff62886148c78ba6
Kabil M <kabilm1012@gmail.com>
2024-10-12 21:35:22 +0530

    uploading package to pypi

--- main.py
+++
@@ -1,5 +0,0 @@
-import invoice
-
-invoice.generate("invoices", "PDFs", "msk.jpg", 
-                 "product_id", "product_name", "amount_purchased",
-                 "price_per_unit", "total_price")
